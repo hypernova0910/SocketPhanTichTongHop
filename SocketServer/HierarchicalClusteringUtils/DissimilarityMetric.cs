3bd6095b62b3ba1e176af6a3dba9007ee9b5689a
hypernova0910 <ngminhhieu2000910@gmail.com>
2022-04-27 16:13:03 +0700

    kmean + phân cụm phân cấp

---
+++ SocketServer/HierarchicalClusteringUtils/DissimilarityMetric.cs
@@ -0,0 +1,18 @@
+﻿using Aglomera;
+using gg.Mesh;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer.HierarchicalClusteringUtils
+{
+    class DissimilarityMetric : IDissimilarityMetric<Vertex>
+    {
+        public double Calculate(Vertex instance1, Vertex instance2)
+        {
+            return Math.Sqrt(Math.Pow(instance1.X - instance2.X, 2) + Math.Pow(instance1.Y - instance2.Y, 2));
+        }
+    }
+}

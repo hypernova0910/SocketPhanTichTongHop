1a762240eea310faa8e171b68e7096b163c73914
hypernova0910 <ngminhhieu2000910@gmail.com>
2022-05-12 10:12:06 +0700

    ngưỡng bom

--- SocketServer/PhanTichBom.cs
+++ SocketServer/PhanTichBom.cs
@@ -884,13 +884,13 @@ namespace Delaunay
                             //Console.WriteLine(bieuDoTungRanh[h].Z);
 
                             //Lấy điểm đầu tiên đi qua khoảng delta
-                            if (bieuDoTungRanh[h].Z > DELTA_STANDARD && markn == DISTRIBUTION_COUNT + 10)
+                            if (bieuDoTungRanh[h].Z > nguong && markn == DISTRIBUTION_COUNT + 10)
                             {
                                 markn = h;
                                 diem1 = bieuDoTungRanh[h];
                             }
                             //Lấy điểm xuống ngay sau điểm lên đó
-                            if (bieuDoTungRanh[h].Z < DELTA_STANDARD && h > markn && diem1 != new Vertex())
+                            if (bieuDoTungRanh[h].Z < nguong && h > markn && diem1 != new Vertex())
                             {
                                 diem2 = bieuDoTungRanh[h];
                                 if (diem1 != new Vertex() && diem2 != new Vertex())
